d6fe874d2e19122baaa355f7320fb6670bb86ba2
gittne <gustav.eriksson6@gmail.com>
2022-12-17 22:58:02 +0100

    Fixed fake enemy bug in level 3

--- 7DFPS_Artware/Assets/Scripts/Target.cs
+++ 7DFPS_Artware/Assets/Scripts/Target.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class Target : MonoBehaviour
 {
     bool isAlive;
     [SerializeField] float health;
+    [SerializeField] bool isPlayer;
     [SerializeField] bool isHuman;
     [SerializeField] bool isFakeSoldier;
     [SerializeField] GameObject soldierModel;
@@ -17,8 +19,10 @@ public class Target : MonoBehaviour
     [SerializeField] GameObject gunToDrop;
     [SerializeField] GameObject enemyPistolModel;
     [SerializeField] GameObject enemySMGModel;
+    [SerializeField] GameObject FakeSoldier;
     EnemyCombat EnemyCombat;
     Vector3 soldierPosition;
+
 
     private void Start()
     {
@@ -27,12 +31,18 @@ public class Target : MonoBehaviour
 
     private void Update()
     {
-        soldierPosition = GameObject.Find("EnemySoldierFake").transform.position;
+        soldierPosition = FakeSoldier.transform.position;
     }
 
     public void TakeDamage(float amountOfDmg)
     {
         health -= amountOfDmg;
+
+        if (health <= 0 && isPlayer)
+        {
+            SceneManager.LoadScene(4);
+        }
+
         if (health <= 0 && !isHuman)
         {
             Destroy(gameObject);
